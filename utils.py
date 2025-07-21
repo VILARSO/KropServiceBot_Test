22ed040967f1060b6eefd0a7a9448cee2861b2f2
VILARSO <vilarso18@gmail.com>
2025-07-21 23:47:44 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.dispatcher import FSMContext
 from aiogram.utils.exceptions import MessageNotModified, MessageToDeleteNotFound, BadRequest
 from aiogram import Bot # Імпортуємо Bot для типізації
+import motor.motor_asyncio # Додано імпорт motor для get_next_sequence_value
 
 # Регулярний вираз для перевірки номера телефону (приклад: +380XXXXXXXXX)
 # Це вже використовується в main.py, але залишено тут як приклад, якщо потрібно буде знову
@@ -99,6 +100,6 @@ async def get_next_sequence_value(db_obj, sequence_name: str) -> int:
         {'_id': sequence_name},
         {'$inc': {'sequence_value': 1}},
         upsert=True,  # Створити, якщо не існує
-        return_document=motor.motor_asyncio.ReturnDocument.AFTER
+        return_document=motor.motor_asyncio.ReturnDocument.AFTER # Тут використовується motor.motor_asyncio
     )
     return result['sequence_value']
