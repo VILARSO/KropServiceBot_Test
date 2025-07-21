99d39dfcfaf527c1fcd4c51d14d272b775ddf5b9
VILARSO <vilarso18@gmail.com>
2025-07-21 22:57:33 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,16 @@
 
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class AppStates(StatesGroup):
+    MAIN_MENU = State() # Головне меню
+    ADD_TYPE = State()
+    ADD_CAT = State()
+    ADD_DESC = State()
+    ADD_CONT = State()
+    ADD_CONFIRM = State()
+
+    VIEW_CAT = State()
+    VIEW_LISTING = State() # Цей стан тепер знову для пагінації загальних оголошень
+    
+    MY_POSTS_VIEW = State()
+    EDIT_DESC = State()
